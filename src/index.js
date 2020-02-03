d9b071ae12cb01f9ad71fe91f5fae1d2af4ad6d5
Lucas Akira Uehara <lucas.uehara@riachuelo.com.br>
2020-02-03 13:20:26 -0300

    fix: trigger hook to update state

--- src/index.js
+++ src/index.js
@@ -4,20 +4,17 @@ const useMediaQueries = (queries, values, defaultValue) => {
   const mediaQueryLists = queries.map((q) => window.matchMedia(q));
 
   const getValue = useCallback(() => {
-    const index = mediaQueryLists.findIndex(({ matches }) => matches);
+    const index = mediaQueryLists.findIndex((mql) => mql.matches);
     return typeof values[index] !== 'undefined' ? values[index] : defaultValue;
   }, [defaultValue, mediaQueryLists, values]);
-
   const [value, setValue] = useState(getValue);
 
-  useLayoutEffect(
-    () => {
-      const handler = () => setValue(getValue);
-      mediaQueryLists.forEach((mediaQuery) => mediaQuery.addListener(handler));
-      return () => mediaQueryLists.forEach((mediaQuery) => mediaQuery.removeListener(handler));
-    },
-    [getValue, mediaQueryLists],
-  );
+  useLayoutEffect(() => {
+    const handler = () => setValue(getValue);
+    mediaQueryLists.forEach((mql) => mql.addListener(handler));
+    return () => mediaQueryLists.forEach((mql) => mql.removeListener(handler));
+    // eslint-disable-next-line
+  }, []);
 
   return value;
 };
